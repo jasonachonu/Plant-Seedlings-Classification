f2e9417eb2dcc4dfcaf638ddcadd016ca904a2a4
Michael Arango <michael.arango08@gmail.com>
2017-12-10 21:03:15 -0500

    Update img_preprocessing.py

--- img_preprocessing.py
+++ img_preprocessing.py
@@ -1,8 +1,9 @@
 def preprocess_image(img):
     """
-	A function to convert the colour image into the segmented image for better feature extraction.
-	"""
-    # Convert the Image from BGR to HSV.
+    A function to convert the colour image into the segmented image for better feature extraction.
+    """
+
+    # Convert the Image from BGR to HSV
     image_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     sensitivity = 35
@@ -10,9 +11,9 @@ def preprocess_image(img):
     upper_hsv = np.array([60 + sensitivity, 255, 255])
     # Creating a mask 
     mask = cv2.inRange(image_hsv, lower_hsv, upper_hsv)
-    # Creating an Elliptical Kernel.
+    # Creating an Elliptical Kernel
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
-    # Remove noise from the image.
+    # Remove noise from the image
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     mask = create_mask(img)
     # Use mask to create a Segmented Image
