fe6adf0300db033f6e3a6f51e3e7b9fb325521c8
Michael Arango <michael.arango08@gmail.com>
2017-12-12 15:39:32 -0500

    Update img_preprocessing.py

--- img_preprocessing.py
+++ img_preprocessing.py
@@ -24,3 +24,16 @@ def preprocess_image(img):
     # Use mask to create a Segmented Image
     output = cv2.bitwise_and(img, img, mask=mask)
     return output
+
+
+def read_img(file_path, size):
+    """
+    A function to read in an input image, rescale the image to a set target size, and convert
+    the image to an array.
+    :param file_path: The path to the image in either the training or test set folder
+    :param size: the target image size to rescale to
+    :return: Returns the rescaled image as an array
+    """
+    img = image.load_img(os.path.join(PATH, file_path), target_size=size)
+    img = image.img_to_array(img)
+    return img
